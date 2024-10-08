d31362e8fe08dbc67c0756b02509eba36f20fa3b
chani-s <slaterchana@gmail.com>
2024-10-09 01:11:47 +0300

    add transitions

--- js/app.js
+++ js/app.js
@@ -43,7 +43,10 @@ function buildBoard() {
 
 			// Place Walls at edges
 			if (i === 0 || i === board.length - 1 || j === 0 || j === board[0].length - 1) {
-				cell.type = WALL;
+				if (i === 3 || j === 2) // Place a transition cell
+					cell.type = FLOOR;
+				else
+					cell.type = WALL;
 			}
 
 			// Add created cell to The game board
@@ -105,7 +108,7 @@ function getRandomInt(min, max) {
 
 // Display a ball in a random location every 2 seconds using setInterval
 function displayRandomBall() {
-    if (isGameOver) return; // Stop adding balls if the game is over
+	if (isGameOver) return;
 
 	var i = getRandomInt(1, gBoard.length - 2);
 	var j = getRandomInt(1, gBoard[0].length - 2);
@@ -122,57 +125,59 @@ function displayRandomBall() {
 function moveTo(i, j) {
     if (isGameOver) return; // Stop adding balls if the game is over
 
-	var targetCell = gBoard[i][j];
-	if (targetCell.type === WALL) return;
+    var targetCell = gBoard[i][j];
 
-	// Calculate distance to make sure we are moving to a neighbor cell
-	var iAbsDiff = Math.abs(i - gGamerPos.i);
-	var jAbsDiff = Math.abs(j - gGamerPos.j);
+    if (targetCell.type === WALL) return;
 
-	// If the clicked Cell is one of the four allowed
-	if ((iAbsDiff === 1 && jAbsDiff === 0) || (jAbsDiff === 1 && iAbsDiff === 0)) {
+    // Bypass the normal distance check for transition cells
+    if (!isTransition(i, j)) {
+        // Calculate distance to make sure we are moving to a neighbor cell
+        var iAbsDiff = Math.abs(i - gGamerPos.i);
+        var jAbsDiff = Math.abs(j - gGamerPos.j);
 
-		if (targetCell.gameElement === BALL) {
-			// Remove the ball from the board after collecting
-			targetCell.gameElement = null;
-			renderCell({ i: i, j: j }, '');
-
-			console.log('Collecting!');
-			collectedBallsCount++;
-			updateCollectedBallsDisplay();
-		}
-
-		// MOVING from current position
-		// Model:
-		gBoard[gGamerPos.i][gGamerPos.j].gameElement = null;
-		// Dom:
-		renderCell(gGamerPos, '');
-
-		// MOVING to selected position
-		// Model:
-		gGamerPos.i = i;
-		gGamerPos.j = j;
-		gBoard[gGamerPos.i][gGamerPos.j].gameElement = GAMER;
-		// DOM:
-		renderCell(gGamerPos, GAMER_IMG);
-
-		// Check if all balls are collected after moving
-		if (checkAllBallsCollected()) {
-            setTimeout(function () {
-                alert('WIN!!! You collected all the balls!');
-                clearInterval(intervalId); 
-				isGameOver = true; 
-            }, 100); // Slight delay to allow last move to complete
+        // If the clicked Cell is not one of the four allowed, return
+        if (!((iAbsDiff === 1 && jAbsDiff === 0) || (jAbsDiff === 1 && iAbsDiff === 0))) {
+            return;
         }
+    }
 
-	} // else console.log('TOO FAR', iAbsDiff, jAbsDiff);
+    // Collect a ball if present
+    if (targetCell.gameElement === BALL) {
+        targetCell.gameElement = null;
+        renderCell({ i: i, j: j }, '');
 
+        console.log('Collecting!');
+        collectedBallsCount++;
+        updateCollectedBallsDisplay();
+    }
+
+    // MOVING from current position
+    gBoard[gGamerPos.i][gGamerPos.j].gameElement = null;
+    renderCell(gGamerPos, '');
+
+    // MOVING to selected position
+    gGamerPos.i = i;
+    gGamerPos.j = j;
+    gBoard[gGamerPos.i][gGamerPos.j].gameElement = GAMER;
+    renderCell(gGamerPos, GAMER_IMG);
+
+    // Check if all balls are collected after moving
+    if (checkAllBallsCollected()) {
+        setTimeout(function () {
+            alert('WIN!!! You collected all the balls!');
+            clearInterval(intervalId);
+            isGameOver = true;
+        }, 100); // Slight delay to allow last move to complete
+    }
 }
+
+
 
 function updateCollectedBallsDisplay() {
 	var elCollectedBalls = document.querySelector('.collected-balls-count');
 	elCollectedBalls.innerText = 'Balls Collected: ' + collectedBallsCount;
 }
+
 
 // Check if all balls are collected
 function checkAllBallsCollected() {
@@ -186,10 +191,12 @@ function checkAllBallsCollected() {
 	return true; // All balls are collected
 }
 
+
 // Reset the game
 function resetGame() {
 	initGame();
 }
+
 
 // Convert a location object {i, j} to a selector and render a value in that element
 function renderCell(location, value) {
@@ -198,30 +205,59 @@ function renderCell(location, value) {
 	elCell.innerHTML = value;
 }
 
+
 // Move the player by keyboard arrows
 function handleKey(event) {
+    var i = gGamerPos.i;
+    var j = gGamerPos.j;
 
-	var i = gGamerPos.i;
-	var j = gGamerPos.j;
+    switch (event.key) {
+        case 'ArrowLeft':
+            [i, j] = checkIfTransition(i, j - 1); // Try moving left
+            break;
+        case 'ArrowRight':
+            [i, j] = checkIfTransition(i, j + 1); // Try moving right
+            break;
+        case 'ArrowUp':
+            [i, j] = checkIfTransition(i - 1, j); // Try moving up
+            break;
+        case 'ArrowDown':
+            [i, j] = checkIfTransition(i + 1, j); // Try moving down
+            break;
+    }
 
-
-	switch (event.key) {
-		case 'ArrowLeft':
-			moveTo(i, j - 1);
-			break;
-		case 'ArrowRight':
-			moveTo(i, j + 1);
-			break;
-		case 'ArrowUp':
-			moveTo(i - 1, j);
-			break;
-		case 'ArrowDown':
-			moveTo(i + 1, j);
-			break;
-
-	}
-
+    // Move to the new position after transition check
+    moveTo(i, j);
 }
+
+
+function checkIfTransition(i, j) {
+    var maxRow = gBoard.length - 1;
+    var maxCol = gBoard[0].length - 1;
+
+    // Transition in horizontal direction (left-right)
+    if (i === 3 && j < 0) {
+        j = maxCol; // Move to the last column on row 3
+    } else if (i === 3 && j > maxCol) {
+        j = 0; // Move to the first column on row 3
+    }
+
+    // Transition in vertical direction (top-bottom)
+    if (j === 2 && i < 0) {
+        i = maxRow; // Move to the last row on column 2
+    } else if (j === 2 && i > maxRow) {
+        i = 0; // Move to the first row on column 2
+    }
+
+    return [i, j];
+}
+
+// Check if the player is on a transition cell
+function isTransition(i, j) {
+    return (i === 3 && (j === 0 || j === gBoard[0].length - 1)) ||
+           (j === 2 && (i === 0 || i === gBoard.length - 1));
+}
+
 
 // Returns the class name for a specific cell
 function getClassName(location) {
